51582e415c13be1d44c56e81490a0cf47ea45c13
David McClure <davidwilliammcclure@gmail.com>
2014-04-24 10:10:25 -0700

    Adds a basic development rig - gulp, browserify, etc.

---
+++ src/heatmap.js
@@ -0,0 +1,8 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80: */
+
+var $ = require('jquery');
+var _ = require('underscore');
+var d3 = require('d3');
+
+console.log('heatmap');
