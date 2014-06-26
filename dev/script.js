84da17b6abf52f44134d31248e4d3b7d83adaddf
David McClure <davidwilliammcclure@gmail.com>
2014-06-26 12:43:08 -0700

    Starts to sketch in the selection logic for the page segments.

--- dev/script.js
+++ dev/script.js
@@ -1,6 +1,7 @@
 
 
 var Heatmap = require('../lib/heatmap');
+var HeatmapSegments = require('../lib/plugins/segments');
 var HeatmapSpans = require('../lib/plugins/spans');
 var $ = require('jquery');
 
@@ -15,9 +16,12 @@ $(function() {
   position();
 
   var heatmap = new Heatmap('#source', '#target');
-  var spans = new HeatmapSpans(heatmap);
 
+  var spans = new HeatmapSpans(heatmap);
   spans.addGroup('slugs', '[data-neatline-slug]');
+
+  var segments = new HeatmapSegments(heatmap);
+  segments.addGroup('pages', 'span[page]');
 
   $(window).resize(function() {
     heatmap.updateSize();
