08bf27c969f06237ff221cb6676fb38164f83757
David McClure <davidwilliammcclure@gmail.com>
2014-06-26 18:49:28 -0700

    Adds a getScaleRatio method to Heatmap.

--- dev/script.js
+++ dev/script.js
@@ -1,6 +1,7 @@
 
 
 var Heatmap = require('../lib/heatmap');
+var Window = require('../lib/window');
 var SegmentGroup = require('../lib/segment-group');
 var SpanGroup = require('../lib/span-group');
 var $ = require('jquery');
@@ -17,11 +18,16 @@ $(function() {
 
   var heatmap = new Heatmap('#source', '#target');
 
+  // Pages.
   var segments = new SegmentGroup(heatmap);
   segments.addGroup('pages', 'span[page]');
 
+  // Slugs.
   var spans = new SpanGroup(heatmap);
   spans.addGroup('slugs', '[data-neatline-slug]');
+
+  // Window.
+  new Window(heatmap);
 
   $(window).resize(function() {
     heatmap.updateSize();
